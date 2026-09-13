b245e541367316f50aa3a5e87101b4fed81a8607
SaudCEO <sauduk11@gmail.com>
2026-09-13 21:07:53 +0300

    Improve best rated movie discovery

--- src/routes/search.js
+++ src/routes/search.js
@@ -500,7 +500,7 @@ const discoverMovies =
        * Avoid tiny-vote 10/10 titles.
        */
       params["vote_count.gte"] =
-        50;
+        500;
     } else if (
       filters.sort === "newest"
     ) {
